1c72c0783a187d16df9b07cb62f62b2b1bf41542
Sowrato Luz Cordeiro <sowrato1234@gmail.com>
2024-11-23 10:25:50 -0300

    Acompanhando a aula fiz um programa que calculava a média entre duas notas e no final mostrava através de uma condição de IF e ELSE se o aluno passou ou não

---
+++ Aulas/Programa simples de Condição/condicao.py
@@ -0,0 +1,23 @@
+# Programa que calcula a média entre duas notas usando condições pra decidir se o aluno passou ou não
+print('\n','-'*5,'MÉDIA ENTRE DUAS NOTAS','-'*5,'\n')
+
+# Pegando as informações do aluno
+name = str(input('Qual é o seu nome? ')).title().strip()
+
+# Pedindo as notas
+nota1 = float(input(f'\n{name}, me informa a sua primeira nota: '))
+nota2 = float(input('\nAgora me diga sua segunda nota: '))
+
+# Cálculo da média
+media = (nota1 + nota2) / 2
+
+print(f'\nSua média {name}, foi de {media}')
+
+# Mostrado se o aluno passo ou não
+if media < 6.0:
+    print(f'\nVocê não atingiu a nota corte, estude mais!!')
+elif media >= 6.0:
+    print(f'\nParabéns {name}, você atingiu a nota corte!!')
+
+# Fim do programa
+print('\n','-'*5,'FIM DO PROGRAMA','-'*5,'\n')
