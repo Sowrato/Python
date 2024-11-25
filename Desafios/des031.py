4cfa1f7dd1afd9f557971ced884a4a860c538d21
Sowrato Luz Cordeiro <sowrato1234@gmail.com>
2024-11-25 16:05:46 -0300

    Desafio 31 feito, calculando o valor de uma viagem de acordo com a distância e com alteração no valor por km.

---
+++ Desafios/des031.py
@@ -0,0 +1,26 @@
+print('\n','-'*5,'DESAFIO 001','-'*5,'\n')
+
+# Pegando a distância
+dis = float(input('Qual a distância em km da sua viagem? '))
+
+# Condição de valores
+if dis > 200:
+    # Avisando sobre o valor a ser cobrado
+    print(f'\nComo sua viagem passa de 200 km, o preço a ser cobrado será R$0,45 centavos por cada km.')
+    
+    # Calculando
+    val_km = dis * 0.45
+
+    # Passando preço da viagem
+    print(f'\nSua viagem custará R${int(val_km)} reais.')
+else:
+    # Avisando sobre o valor a ser cobrado
+    print(f'Como sua viagem não passa de 200 km, o preço a ser cobrado será R$0,50 centavos por cada km.')
+    
+    # Calculando
+    val_km = dis * 0.50
+    
+    # Passando preço da viagem
+    print(f'\nSua viagem custará R${int(val_km)} reais.')
+
+print('\n','-'*5,'FIM DO PROGRAMA','-'*5,'\n')
