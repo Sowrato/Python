1f7140e63445169e9401aad6c94a4dc7b1ea9489
Sowrato Luz Cordeiro <sowrato1234@gmail.com>
2024-12-14 10:35:12 -0300

    Acompanhando a aula 11 do Curso em Vídeo de Python, fiz as anotações das formatações básicas que são possíveis fazer no terminal.

---
+++ Aulas/Cores no Terminal/cores.py
@@ -0,0 +1,36 @@
+# FORMÚLA PRA CORES
+
+# \033[0;33;44m   FORMÚLA
+
+# 0 --> estilo
+# 33 --> cor de texto
+# 44 --> cor de fundo
+
+# ESTILOS
+
+# 0 --> None
+# 1 --> Bold
+# 4 --> Underline
+# 7 --> Negativo
+
+# TEXTOS
+
+# 30 --> BRANCO
+# 31 --> VERMELHO
+# 32 --> VERDE
+# 33 --> AMARELO
+# 34 --> ROSA
+# 35 --> ROXO
+# 36 --> CIANO
+# 37 --> CINZA
+
+# FUNDO
+
+# 40 --> BRANCO
+# 41 --> VERMELHO
+# 42 --> VERDE
+# 43 --> AMARELO
+# 44 --> ROSA
+# 45 --> ROXO
+# 46 --> CIANO
+# 47 --> CINZA
