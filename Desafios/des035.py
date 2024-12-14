78229a3a7d811f6f5daa8f4f1a1a48351dab9531
Sowrato Luz Cordeiro <sowrato1234@gmail.com>
2024-12-14 09:35:52 -0300

    Não estava conseguindo fazer corretamente o desafio, então acompanhando a aula do desafio 35, eu pude corrigir os erros!

--- Desafios/des035.py
+++ Desafios/des035.py
@@ -9,13 +9,9 @@ reta2 = float(input('\n2. Informe o tamanho em metros da segunda reta: '))
 reta3 = float(input('\n3. Informe o tamanho em metros da terceira reta: '))
 
 # Condições
-if reta1 < (reta2/2) and reta1 < (reta3/2):
-    print(f'\nÉ possível fazer um triângulo.')
-elif reta2 < (reta1/2) and reta2 < (reta3/2):
-    print(f'\nÉ possível fazer um triângulo.')
-elif reta3 < (reta1/2) and reta3 < (reta2/2):
+if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
     print(f'\nÉ possível fazer um triângulo.')
 else:
-    print(f'\nNão é possível fazer um triângulo.')
+    print(f'\nNÃO é possível fazer um triângulo.')
 
 print('\n','-'*5,'FIM DO PROGRAMA','-'*5,'\n')
