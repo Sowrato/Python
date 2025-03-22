204329030e86c76c22eb3998d706ca9eba3be5b6
Sowrato Luz Cordeiro <sowrato1234@gmail.com>
2025-03-22 11:33:48 -0300

    Desafio feito

--- Desafios/des038.py
+++ Desafios/des038.py
@@ -1,5 +1,15 @@
 print('\n','-'*5,'DESAFIO 038','-'*5,'\n')
 
+n1 = int(input('Me diga o primeiro número: '))
+n2 = int(input('\nAgora o segundo número: '))
 
+if n1 > n2:
+    print(f'\nO primeiro número {n1} é maior que o segundo {n2}.')
+
+elif n1 < n2:
+    print(f'\nO segundo número {n2} é maior que o primeiro {n1}.')
+
+elif n1 == n2:
+    print(f'\nOs número são iguais.')
 
 print('\n','-'*5,'FIM DO PROGRAMA','-'*5,'\n')
