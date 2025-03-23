ae9a3d9e56d2c002c3c0696989e9c84a52c2ef27
Sowrato Luz Cordeiro <sowrato1234@gmail.com>
2025-03-23 10:45:42 -0300

    Desafio 41 finalizado

--- Desafios/des041.py
+++ Desafios/des041.py
@@ -1,5 +1,28 @@
+from datetime import date
+
+# Pegando o ano;
+ano = date.today().year
+
 print('\n','-'*5,'DESAFIO 041','-'*5,'\n')
 
+ano_nasc = int(input('Me informe o ano de nascimento do atleta: '))
+idade = ano - ano_nasc
 
+if idade <= 9:
+    print(f'\nO atleta tem {idade} anos e está na categoria MIRIM!')
+elif idade <= 14:
+    print(f'\nO atleta tem {idade} anos e está na categoria INFANTIL!')
+elif idade <= 19:
+    print(f'\nO atleta tem {idade} anos e está na categoria JUNIOR!')
+elif idade <= 20:
+    print(f'\nO atleta tem {idade} anos e está na categoria SÊNIOR!')
+else:
+    print(f'\nO atleta tem {idade} anos e está na categoria MASTER!')
 
 print('\n','-'*5,'FIM DO PROGRAMA','-'*5,'\n')
+
+# até 9 anos: Mirim
+# até 14 anos: Infantil
+# até 19 anos: Junior
+# até 20 anos: Sênior
+# acima: Master
