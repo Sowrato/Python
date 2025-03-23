bbfb263e58090f959a4bb3eef5cd3b939434640b
Sowrato Luz Cordeiro <sowrato1234@gmail.com>
2025-03-23 10:32:52 -0300

    Desafio 40 realizado!

--- Desafios/des040.py
+++ Desafios/des040.py
@@ -1,5 +1,22 @@
-print('\n','-'*5,'DESAFIO 039','-'*5,'\n')
+print('\n','-'*5,'DESAFIO 040','-'*5,'\n')
 
+# Pedindo as notas;
+nota1 = float(input('Me informe sua primeira nota: '))
+nota2 = float(input('\nMe informe sua segunda nota: '))
 
+# Calculando a média;
+media = (nota1 + nota2) / 2
+
+# Condições;
+if media < 5.0:
+    print(f'\nInfelizmente você ficou abaixo da média com {media}, então está REPROVADO!')
+elif media < 7.0:
+    print(f'\nCom a sua média de {media}, você está de RECUPERAÇÃO!')
+elif media >= 7.0:
+    print(f'\nParabéns, você atingiu a média com {media} e está APROVADO!')
+
+# abaixo 5.0
+# entre 5.0 e 6.9
+# igual ou acima 7.0
 
 print('\n','-'*5,'FIM DO PROGRAMA','-'*5,'\n')
